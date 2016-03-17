ec41930d14fd66a3e357271e61d63334401d061c
renanbastos93 <renanbastos.tec@gmail.com>
2016-03-16 22:15:58 -0300

    Inicio do Projeto

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var App = angular.module('App', [])
+App.controller('alunoController', function alunoController($scope) {
+	//Controller do Aluno
+}
+App.controller('empresaController', function empresaController($scope)){
+	//Controller da Empresa
+}
+
+);
